4868c057f3b5f7ca7cae8bf3271a21e551870699
Aarav <virmaniaarav@gmail.com>
2023-09-12 19:45:18 +0530

    updates

--- app.js
+++ app.js
@@ -1,36 +1,37 @@
-document.addEventListener('DOMContentLoaded', () =>{
-const bird=document.querySelector('.bird')
-const gameDisplay= document.querySelector('.game-container')
-const ground=document.querySelector('.ground')
-let birdleft= 220
-let birdbottom=100
-let gravity =2;
-function startGame()
-{
-    birdbottom-=gravity
-    bird.style.bottom= birdbottom + 'px';
-    bird.style.left=birdleft + 'px';
-}
-let TimerId=setInterval(startGame,20)
-function control(e)
-{
-    if(e.keyCode===32)
-    {
-        jump()
+document.addEventListener("DOMContentLoaded", () => {
+    const bird = document.querySelector(".bird");
+    const gameDisplay = document.querySelector(".game-container");
+    const ground = document.querySelector(".ground");
+    let birdleft = 220;
+    let birdbottom = 100;
+    let gravity = 2;
+    function startGame() {
+      birdbottom -= gravity;
+      bird.style.bottom = birdbottom + "px";
+      bird.style.left = birdleft + "px";
     }
-}
-function jump()
-{
-    if(birdbottom<500) birdbottom+=50
-    bird.style.bottom=birdBottom + 'px'
-}
-document.addEventListener('keyup',control)
-function generateObstacle()
-{
-    const obstacle =document.createElement('div')
-    obstacle.classlist.add('obstacle')
-    gameDisplay.appendChild(obstacle)
+    let TimerId = setInterval(startGame, 20);
+    function control(e) {
+      if (e.keyCode === 32) {
+        jump();
+      }
+    }
+    function jump() {
+      if (birdbottom < 500) birdbottom += 50;
+      bird.style.bottom = birdbottom + "px";
+    }
+    document.addEventListener("keyup", control);
+  
+    function generateObstacle() {
+      let obstacleLeft=500;
+      let randomHeight =Math.random() * 60;
+     let obstacleBottom=randomHeight;
+      const obstacle = document.createElement("div");
+     obstacle.classList.add("obstacle");
+      gameDisplay.appendChild(obstacle);
+      obstacle.style.left = obstacleLeft+ "px";
+      obstacle.style.bottom=obstacleBottom +"px";
 
-}
-generateObstacle()
-})
+    }
+    generateObstacle();
+  });
